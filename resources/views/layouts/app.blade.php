c1826187cad72a77631a2c98802d84bc818fc850
Setthawut <assetthawut@gmail.com>
2019-11-09 09:37:09 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,11 +48,13 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                {{-- <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a> --}}
+                                <a class="nav-link" href="#">{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    {{-- <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a> --}}
+                                    <a class="nav-link" href="#">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
@@ -62,7 +64,8 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item text-gray" href="{{ route('logout') }}"
+                                    {{-- <a class="dropdown-item text-gray" href="{{ route('logout') }}" --}}
+                                    <a class="dropdown-item text-gray" href="#"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
